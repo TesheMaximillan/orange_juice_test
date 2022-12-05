19ccb2cbbd94a11dc9a4368660ebe894645d9f0f
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 22:58:04 +0300

    Create Registration page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,18 +4,18 @@ class UserController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
 
-    if @user.valid?
-      redirect_to search_items_path, notice: 'User created!'
+    if @user.save
+      redirect_to search_items_path, notice: 'User successfully created.'
     else
-      render :new, alert: 'User not created!'
+      render :new, status: :unprocessable_entity, alert: 'User is not able to created.'
     end
   end
 
   private
 
   def user_params
-    params.permit(:username)
+    params.require(:user).permit(:username)
   end
 end
