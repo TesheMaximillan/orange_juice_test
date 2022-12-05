787e56400eee7611a29fb23904dd333cbcd8e2a8
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 18:36:43 +0300

    Fix rubocop errors

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :request do
-  describe "sessions#create" do
-    it "should login" do
-      user = User.create(username: "test2")
+RSpec.describe 'Sessions', type: :request do
+  describe 'sessions#create' do
+    it 'should login' do
+      user = User.create(username: 'test2')
       post sessions_path, params: { username: user.username }
       expect(response).to have_http_status(302)
     end
   end
 
-  describe "sessions#logout" do
-    it "should logout" do
-      user = User.create(username: "test3")
+  describe 'sessions#logout' do
+    it 'should logout' do
+      user = User.create(username: 'test3')
       post sessions_path, params: { username: user.username }
       delete logout_path
       expect(response).to have_http_status(302)
     end
   end
 
-  describe "sessions#new" do
-    it "should return a 200 response" do
+  describe 'sessions#new' do
+    it 'should return a 200 response' do
       get new_session_path
       expect(response).to have_http_status(200)
     end
