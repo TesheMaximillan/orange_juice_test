db579b400051f8dcfb9e7583307f32a79b0e9919
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 17:53:49 +0300

    Modify Add routing sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/logout'
   root to: 'home#index'
 
   resources :user, only: [:new, :create]
-  post :login, to: 'user#login'
-  delete :logout, to: "user#logout"
+  resources :sessions, only: [:new, :create]
+  delete :logout, to: "sessions#logout"
   resources :search_items, only: [:index, :new, :create]
 end
