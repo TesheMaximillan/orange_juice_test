a55738777b5165e435274773b19f998defb2b6c3
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 16:21:11 +0300

    Create search items controller method

---
+++ app/controllers/search_items_controller.rb
@@ -0,0 +1,32 @@
+class SearchItemController < ApplicationController
+  include CurrentUserConcern
+  def index
+    @search_items = SearchItem.all.where(user: @current_user).order(rank: :desc)
+    @search_items.map { |search_item|
+                   search_item.attributes.except('user_id', 'created_at', 'updated_at')
+                 }
+  end
+
+  def create
+    if search_item_exists?
+      @search_item = SearchItem.all.where(user: @current_user).where(text: search_item_params[:text]).first
+      @search_item.update(rank: @search_item.rank + 1)
+    else
+      @search_item = SearchItem.new(search_item_params)
+      @search_item.user_id = @current_user.id
+      @search_item.save
+    end
+    render json: @search_item, status: :created
+  end
+
+  private
+
+  def search_item_params
+    params.permit(:text, :rank)
+  end
+
+  # check if the search item already exists
+  def search_item_exists?
+    SearchItem.all.where(user: @current_user).where(text: search_item_params[:text]).exists?
+  end
+end
--- spec/requests/search_items_spec.rb
+++ spec/requests/search_items_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "SearchItems", type: :request do
+  before(:each) do
+    @user = create(:user)
+    @search_item = create(:search_item, user: @user)
+  end
   describe "GET /index" do
     it "returns http success" do
       get "/search_item/index"
