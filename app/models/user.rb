89275add28ac07b9ab39e8b07a43a742e2c0783e
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 10:35:00 +0300

    Capitalize & trim username before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_many :search_items
+  before_save :capitalize_name
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+
+  #Remove whitespace and capitalize name
+  def capitalize_name
+    self.username = username.strip.capitalize
+  end
 end
