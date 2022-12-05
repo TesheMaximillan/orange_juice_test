74633e658f65f8722004b548c05d4e7a86a76757
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 10:18:18 +0300

    Test user with minimum character

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe User, type: :model do
     it 'is invalid without a username' do
       expect(build(:user, username: nil)).to_not be_valid
     end
+
+    it 'is invalid with a username that is too short' do
+      expect(build(:user, username: 'a')).to_not be_valid
+    end
   end
 end
