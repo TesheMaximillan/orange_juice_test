80ac61261ceb2a914da1eca2325816afcfb1a3b2
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 18:01:23 +0300

    Test 'user#new'

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -3,13 +3,25 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'home#index' do
     it 'should return a 200 response' do
-      get '/'
+      get root_path
       expect(response).to have_http_status(200)
     end
 
     it 'should render the index template' do
-      get '/'
+      get root_path
       expect(response).to render_template('index')
+    end
+  end
+
+  describe 'users#new' do
+    it 'should return a 200 response' do
+      get new_user_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render the new template' do
+      get new_user_path
+      expect(response).to render_template('new')
     end
   end
 end
