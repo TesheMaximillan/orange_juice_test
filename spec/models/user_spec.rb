9fceb3cefc438e1418696af904a4cabb2692e0bb
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 10:17:19 +0300

    Test user with invalid username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe User, type: :model do
     it 'has a valid factory' do
       expect(build(:user)).to be_valid
     end
+
+    it 'is invalid without a username' do
+      expect(build(:user, username: nil)).to_not be_valid
+    end
   end
 end
