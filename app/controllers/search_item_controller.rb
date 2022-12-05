95818830ce033d60b51462418fadb40e470a536c
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 15:55:34 +0300

    Return search_items for the current user

--- app/controllers/search_item_controller.rb
+++ app/controllers/search_item_controller.rb
@@ -1,7 +1,18 @@
 class SearchItemController < ApplicationController
+  include CurrentUserConcern
   def index
+    @search_items = SearchItem.all.where(user: @current_user).order(rank: :desc)
+    @search_items.map { |search_item|
+                   search_item.attributes.except('user_id', 'created_at', 'updated_at')
+                 }
   end
 
   def create
+  end
+
+  private
+
+  def search_item_params
+    params.permit(:text, :rank)
   end
 end
