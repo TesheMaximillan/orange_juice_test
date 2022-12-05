f11019b57252e4ade8f2ac52f985433d5284cc3f
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 10:15:12 +0300

    Test for valid factory

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_063721) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_064630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "search_items", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "text", null: false
+    t.integer "rank", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["text"], name: "index_search_items_on_text", unique: true
+    t.index ["user_id"], name: "index_search_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -21,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_063721) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "search_items", "users"
 end
