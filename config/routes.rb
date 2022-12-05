d2e661cc10210e0cf2ac13dfc572f15957c3fde3
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 17:22:15 +0300

    Add routes to home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'user#login', as: 'login'
+  root to: 'home#index'
+
   resources :user, only: [:new, :create]
+  post :login, to: 'user#login'
   delete :logout, to: "user#logout"
   if :user
-    resources :search_items, only: [:index, :create]
+    resources :search_items, only: [:index, :new, :create]
   end
 end
