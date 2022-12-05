64ab2a18ac13896f365a75e906d480291e6e7048
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 13:40:59 +0300

    Generate user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/show'
+  get 'user/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
