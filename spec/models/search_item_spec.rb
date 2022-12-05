95d82515d72f8519c1b0860c0cd941914f99d5ae
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-05 10:47:24 +0300

    Test text with too long character

--- spec/models/search_item_spec.rb
+++ spec/models/search_item_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe SearchItem, type: :model do
     it 'is invalid with text that is too short' do
       expect(build(:search_item, text: 'a')).to_not be_valid
     end
+
+    it 'is invalid with text that is too long' do
+      expect(build(:search_item, text: 'a' * 201)).to_not be_valid
+    end
   end
 end
