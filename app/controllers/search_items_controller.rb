d0109cc8c826bfb205b408cf689b5337e44c4108
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-06 15:41:31 +0300

    Add search functionality for create action

--- app/controllers/search_items_controller.rb
+++ app/controllers/search_items_controller.rb
@@ -5,29 +5,34 @@ class SearchItemsController < ApplicationController
     @search_item = SearchItem.new
   end
 
-  def index
-    @search_items = SearchItem.all.where(user: @current_user).order(rank: :desc)
-    @search_items.map do |search_item|
-      search_item.attributes.except('user_id', 'created_at', 'updated_at')
-    end
-  end
-
   def create
-    if search_item_exists?
-      @search_item = SearchItem.all.where(user: @current_user).where(text: search_item_params[:text].strip.capitalize).first
-      @search_item.update(rank: @search_item.rank + 1)
+    if params[:text].present? && params[:text].end_with?('?')
+      if search_item_exists?
+        @search_item = SearchItem.all.where(user: @current_user).where(text: search_item_params[:text].strip.capitalize).first
+        @search_item.update(rank: @search_item.rank + 1)
+      else
+        @search_item = SearchItem.new(search_item_params)
+        @search_item.user_id = @current_user.id
+        @search_item.save
+      end
     else
-      @search_item = SearchItem.new(search_item_params)
-      @search_item.user_id = @current_user.id
-      @search_item.save
-    end
-    render json: @search_item, status: :created
-  end
+      if params[:text].present?
+        text = params[:text].strip.capitalize
+        @search_items = SearchItem.all.where(user: @current_user).where('text LIKE ?', "%#{text}%").order(rank: :desc)
+      else
+        @search_items = [];
+      end
 
-  private
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update('search_results', partial: 'search_items/search_results', locals: { search_items: @search_items })
+        end
+      end
+    end
+  end
 
   def search_item_params
-    params.require(:search_item).permit(:text)
+    params.permit(:text)
   end
 
   # check if the search item already exists
