23a196684bf898e3aac54b184e3588a0e1d327e0
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-06 15:42:41 +0300

    Modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :user, only: [:new, :create]
   resources :sessions, only: [:new, :create]
   delete :logout, to: "sessions#logout"
-  resources :search_items, only: [:index, :new, :create]
+  post :search_items, to: "search_items#create"
+  get :search_items, to: "search_items#new"
 end
